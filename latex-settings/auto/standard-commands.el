e7a01eb9da342e2c90308cff1c4b17ced26cb2e4
Anders Munch <amnudn@gmail.com>
2022-05-10 10:39:25 +0200

    draft slides for third meeting

--- latex-settings/auto/standard-commands.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "standard-commands"
- (lambda ()
-   (TeX-add-symbols
-    "independenT"))
- :plain-tex)
-
--- latex-settings/auto/title-compact.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "title-compact"
- (lambda ()
-   (TeX-run-style-hooks
-    "transparent"))
- :latex)
-
